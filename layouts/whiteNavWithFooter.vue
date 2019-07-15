f78dec8dfff2525cac115ef4b5cab39b541eb963
Iwuanyanwu Chiamaka <amanicholas@gmail.com>
2019-07-15 09:55:17 +0100

    AMA: Created state page

---
+++ layouts/whiteNavWithFooter.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <div>
+    <WhiteNav/>
+    <nuxt />
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import WhiteNav from '~/components/Shared/whiteNavBar'
+import Footer from '~/components/Shared/footer'
+export default {
+    components: {
+        Footer,
+        WhiteNav
+    }
+}
+</script>
