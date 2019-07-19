fd10fecddca39cb00c35069aa674d6db5efcf575
Iwuanyanwu Chiamaka <amanicholas@gmail.com>
2019-07-19 10:50:59 +0100

    AMA: fixed margin

--- components/Shared/footer.vue
+++ components/Shared/footer.vue
@@ -18,7 +18,7 @@
                     <img src="~assets/icons/play-store-icon.svg" alt class="mb-2"/>
                   </a>
                   <a href>
-                    <img src="~assets/icons/app-store-icon.svg" alt />
+                    <img src="~assets/icons/app-store-icon.svg" alt class="mb-2"/>
                   </a>
                 </div>
               </div>
