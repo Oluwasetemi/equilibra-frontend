bb4cd6e43fd69198d3cd2169afa36f3ee3b757ef
Ahman Emmanuel <ahmanemmanuel@gmail.com>
2019-09-11 14:55:48 +0100

    added auth module for nuxt

--- components/Shared/facebookButton.vue
+++ components/Shared/facebookButton.vue
@@ -6,51 +6,31 @@
 </template>
 
 <script>
-window.fbAsyncInit = function() {
-  FB.init({
-    appId: process.env.facebookAppID,
-    cookie: true,
-    xfbml: true,
-    version: "v2.9"
-  });
-
-  FB.AppEvents.logPageView();
-};
-
-(function(d, s, id) {
-  var js,
-    fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {
-    return;
-  }
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-})(document, "script", "facebook-jssdk");
-</script>
-<script>
 export default {
   methods: {
     checkFBLoginStatus() {
-      const self = this;
-      FB.login(
-        function(response) {
-          if (response.status === "connected") {
-            self.$emit(
-              "loggedInWithFacebook",
-              response.authResponse.accessToken
-            );
-            // Logged into your app and Facebook.
-          } else {
-            self.$emit("facebookError", response);
-          }
-        },
-        {
-          scope: "email, user_birthday"
-        }
-      );
+      this.$auth.loginWith('facebook')
+      // const self = this;
+      // FB.login(
+      //   function(response) {
+      //     if (response.status === "connected") {
+      //       self.$emit(
+      //         "loggedInWithFacebook",
+      //         response.authResponse.accessToken
+      //       );
+      //       // Logged into your app and Facebook.
+      //     } else {
+      //       self.$emit("facebookError", response);
+      //     }
+      //   },
+      //   {
+      //     scope: "email, user_birthday"
+      //   }
+      // );
     }
+  },
+  created(){
+    
   },
   head() {
     return {
