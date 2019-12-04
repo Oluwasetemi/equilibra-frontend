caeea48656b43c1f984ce1ef4bd65150dd391c11
Ahman Emmanuel <ahmanemmanuel@gmail.com>
2019-12-04 12:23:47 +0100

    go

--- plugins/apollo.config.js
+++ plugins/apollo.config.js
@@ -20,7 +20,7 @@ export default function(context) {
   return {
     onError: errorLink,
     httpEndpoint: "https://api.equilibra.test.natterbase.com",
-    wsEndpoint: "wss://api.equilibra.test.natterbase.com/subscriptions",
+    wsEndpoint: "wss://api.equilibra.test.natterbase.com:5505/subscriptions",
     persisting: false,
     websocketsOnly: false,
     httpLinkOptions: {
