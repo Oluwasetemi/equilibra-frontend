a30e5085d2dabeba60e794e924f4e9e0bfb3eff2
Natterbase <tech@natterbase.com>
2019-12-19 14:10:26 +0000

    Update apollo.config.js

--- plugins/apollo.config.js
+++ plugins/apollo.config.js
@@ -19,8 +19,8 @@ export default function(context) {
   );
   return {
     onError: errorLink,
-    httpEndpoint: "https://api.equilibra.test.natterbase.com",
-    wsEndpoint: "wss://api.equilibra.test.natterbase.com:5505/subscriptions",
+    httpEndpoint: "https://api-staging.equilibra.test.natterbase.com",
+    wsEndpoint: "wss://api-staging.equilibra.test.natterbase.com:555/subscriptions",
     persisting: false,
     websocketsOnly: false,
     httpLinkOptions: {
