bfd0045eddb6c2d402f90ebababfc63a475c6a8e
Faith Idowu <faithid10@gmail.com>
2020-01-15 18:37:23 +0100

    Fix: changed allRooms government query from id to _id

--- apollo/admin/data.js
+++ apollo/admin/data.js
@@ -1,189 +1,189 @@
 import gql from "graphql-tag";
 
 export default {
-  allGovts: gql`
-    query allGovernmentBasedOnCategory(
-      $start: Int
-      $first: Int
-      $slug: categoryEnum!
-    ) {
-      allGovernmentBasedOnCategory(start: $start, first: $first, slug: $slug) {
-        id
-        name
-        category
-        stateGovernment
-        description
-        slogan
-        slug
-        cjn
-        speaker
-        leader
-        senatePresident
-        totalLg
-        population
-        totalConstituency
-        infantMortalityRate
-        foreignReserve
-        crimeRate
-        inflationRate
-        budgetPerformanceRate
-        budgetPassDate
-        budgetSubmissionDate
-        plenaryAttendanceRate
-        gdpPerHead
-        powerGenerated
-        literacyRate
-        unemploymentRate
-        nonOilSectorContributionToGDP
-        rulingParty
-        touristAttractionCenters
-        agriculture
-        mineralResources
-        museumsAndParks
-        leaderPhoto
-        senatePresidentPhoto
-        cjnPhoto
-        speakerPhoto
-      }
-    }
-  `,
+	allGovts: gql`
+		query allGovernmentBasedOnCategory(
+			$start: Int
+			$first: Int
+			$slug: categoryEnum!
+		) {
+			allGovernmentBasedOnCategory(start: $start, first: $first, slug: $slug) {
+				id
+				name
+				category
+				stateGovernment
+				description
+				slogan
+				slug
+				cjn
+				speaker
+				leader
+				senatePresident
+				totalLg
+				population
+				totalConstituency
+				infantMortalityRate
+				foreignReserve
+				crimeRate
+				inflationRate
+				budgetPerformanceRate
+				budgetPassDate
+				budgetSubmissionDate
+				plenaryAttendanceRate
+				gdpPerHead
+				powerGenerated
+				literacyRate
+				unemploymentRate
+				nonOilSectorContributionToGDP
+				rulingParty
+				touristAttractionCenters
+				agriculture
+				mineralResources
+				museumsAndParks
+				leaderPhoto
+				senatePresidentPhoto
+				cjnPhoto
+				speakerPhoto
+			}
+		}
+	`,
 
-  allRooms: gql`
-    query fetchAllConstituencyAdmin(
-      $limit: Int
-      $skip: Int
-      $roomType: fetchConstituency
-    ) {
-      fetchAllConstituencyAdmin(
-        limit: $limit
-        skip: $skip
-        roomType: $roomType
-      ) {
-        edges {
-          name
-          _id
-          government {
-            id
-            name
-          }
-        }
-        pageInfo {
-          totalCount
-          hasNextPage
-        }
-      }
-    }
-  `,
+	allRooms: gql`
+		query fetchAllConstituencyAdmin(
+			$limit: Int
+			$skip: Int
+			$roomType: fetchConstituency
+		) {
+			fetchAllConstituencyAdmin(
+				limit: $limit
+				skip: $skip
+				roomType: $roomType
+			) {
+				edges {
+					name
+					_id
+					government {
+						_id
+						name
+					}
+				}
+				pageInfo {
+					totalCount
+					hasNextPage
+				}
+			}
+		}
+	`,
 
-  allRoomsByGovt: gql`
-    query fetchConstituency(
-      $roomType: fetchConstituency
-      $stateGovernmentID: ID
-    ) {
-      fetchConstituency(
-        stateGovernmentID: $stateGovernmentID
-        roomType: $roomType
-      ) {
-        name
-        _id
-        government
-      }
-    }
-  `,
+	allRoomsByGovt: gql`
+		query fetchConstituency(
+			$roomType: fetchConstituency
+			$stateGovernmentID: ID
+		) {
+			fetchConstituency(
+				stateGovernmentID: $stateGovernmentID
+				roomType: $roomType
+			) {
+				name
+				_id
+				government
+			}
+		}
+	`,
 
-  updateRoom: gql`
-    mutation updateRoom($room: UpdateRoomInputType, $roomId: darangiGraphId) {
-      updateRoom(room: $room, roomId: $roomId) {
-        _id
-        name
-        government
-      }
-    }
-  `,
+	updateRoom: gql`
+		mutation updateRoom($room: UpdateRoomInputType, $roomId: darangiGraphId) {
+			updateRoom(room: $room, roomId: $roomId) {
+				_id
+				name
+				government
+			}
+		}
+	`,
 
-  createRoom: gql`
-    mutation createRoom($room: RoomInputType!) {
-      createRoom(room: $room) {
-        _id
-        name
-        government
-      }
-    }
-  `,
+	createRoom: gql`
+		mutation createRoom($room: RoomInputType!) {
+			createRoom(room: $room) {
+				_id
+				name
+				government
+			}
+		}
+	`,
 
-  deleteRoom: gql`
-    mutation deleteRoom($roomId: darangiGraphId) {
-      deleteRoom(roomId: $roomId) {
-        _id
-        name
-      }
-    }
-  `,
+	deleteRoom: gql`
+		mutation deleteRoom($roomId: darangiGraphId) {
+			deleteRoom(roomId: $roomId) {
+				_id
+				name
+			}
+		}
+	`,
 
-  allCats: gql`
-    query categories {
-      categories {
-        id
-        name
-        slug
-      }
-    }
-  `,
+	allCats: gql`
+		query categories {
+			categories {
+				id
+				name
+				slug
+			}
+		}
+	`,
 
-  createGovt: gql`
-    mutation updateGovernment($id: ID!, $governmentInput: governmentInput!) {
-      updateGovernment(id: $id, governmentInput: $governmentInput) {
-        id
-        name
-        category
-        stateGovernment
-        description
-        slogan
-        slug
-        cjn
-        speaker
-        leader
-        leaderPhoto
-        speakerPhoto
-        senatePresidentPhoto
-        cjnPhoto
-        senatePresident
-        totalLg
-        population
-        totalConstituency
-        infantMortalityRate
-        foreignReserve
-        crimeRate
-        inflationRate
-        budgetPerformanceRate
-        budgetPassDate
-        budgetSubmissionDate
-        plenaryAttendanceRate
-        gdpPerHead
-        powerGenerated
-        literacyRate
-        unemploymentRate
-        nonOilSectorContributionToGDP
-        rulingParty
-        touristAttractionCenters
-        agriculture
-        mineralResources
-        museumsAndParks
-      }
-    }
-  `,
+	createGovt: gql`
+		mutation updateGovernment($id: ID!, $governmentInput: governmentInput!) {
+			updateGovernment(id: $id, governmentInput: $governmentInput) {
+				id
+				name
+				category
+				stateGovernment
+				description
+				slogan
+				slug
+				cjn
+				speaker
+				leader
+				leaderPhoto
+				speakerPhoto
+				senatePresidentPhoto
+				cjnPhoto
+				senatePresident
+				totalLg
+				population
+				totalConstituency
+				infantMortalityRate
+				foreignReserve
+				crimeRate
+				inflationRate
+				budgetPerformanceRate
+				budgetPassDate
+				budgetSubmissionDate
+				plenaryAttendanceRate
+				gdpPerHead
+				powerGenerated
+				literacyRate
+				unemploymentRate
+				nonOilSectorContributionToGDP
+				rulingParty
+				touristAttractionCenters
+				agriculture
+				mineralResources
+				museumsAndParks
+			}
+		}
+	`,
 
-  suspend: gql`
-    mutation suspendAdmin($adminId: ID!) {
-      suspendAdmin(id: $adminId) {
-        role
-        fullName
-        username
-        email
-        successMessage
-        isSuspended
-        _id
-      }
-    }
-  `
+	suspend: gql`
+		mutation suspendAdmin($adminId: ID!) {
+			suspendAdmin(id: $adminId) {
+				role
+				fullName
+				username
+				email
+				successMessage
+				isSuspended
+				_id
+			}
+		}
+	`
 };
